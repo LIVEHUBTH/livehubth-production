b2c1824385c4302c1017482eabfd928434cbb68f
LIVEHUBTH <livehubth@gmail.com>
2026-08-06 18:55:25 +0700

    Update register.js

--- functions/api/register.js
+++ functions/api/register.js
@@ -1,4 +1,38 @@
-export async function onRequestPost() {
+export async function onRequestPost(context) {
+  const db = context.env.DB;
+
+  const data = await context.request.json();
+
+  const name = data.name;
+  const email = data.email;
+  const password = data.password;
+
+  if (!name || !email || !password) {
+    return Response.json({
+      success: false,
+      message: "Missing data"
+    }, { status: 400 });
+  }
+
+  const exists = await db
+    .prepare("SELECT id FROM users WHERE email = ?")
+    .bind(email)
+    .first();
+
+  if (exists) {
+    return Response.json({
+      success: false,
+      message: "Email already exists"
+    });
+  }
+
+  await db
+    .prepare(
+      "INSERT INTO users(name,email,password) VALUES(?,?,?)"
+    )
+    .bind(name, email, password)
+    .run();
+
   return Response.json({
     success: true
   });
