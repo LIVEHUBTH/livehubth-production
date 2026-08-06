122566070ba7199fb99f73c5b82a6cca0447fd2f
LIVEHUBTH <livehubth@gmail.com>
2026-08-06 19:04:09 +0700

    Update login.js

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,5 +1,27 @@
-export async function onRequestPost() {
+export async function onRequestPost(context) {
+  const db = context.env.DB;
+
+  const data = await context.request.json();
+
+  const email = data.email;
+  const password = data.password;
+
+  const user = await db
+    .prepare(
+      "SELECT id,name,email,role FROM users WHERE email = ? AND password = ?"
+    )
+    .bind(email, password)
+    .first();
+
+  if (!user) {
+    return Response.json({
+      success: false,
+      message: "Invalid email or password"
+    });
+  }
+
   return Response.json({
-    success: true
+    success: true,
+    user
   });
 }
